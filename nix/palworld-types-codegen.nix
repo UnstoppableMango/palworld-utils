8593e126258daac0d42a07db42f8df3588f7af5c
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-08-20 11:54:48 -0500

    FetchFromGitHub

--- nix/palworld-types-codegen.nix
+++ nix/palworld-types-codegen.nix
@@ -6,11 +6,14 @@ let
   # upstream changes PALWORLD_TYPE_HINTS (see src/palworld_types.rs for what
   # consumes the generated table).
   rev = "e6cd688813999f3851e3d16e1e643bfa93865904";
-  paltypesPy = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/deafdudecomputers/PalworldSaveTools/${rev}/src/palsav/palsav/paltypes.py";
-    hash = "sha256-eF8aVqGCxnoknrIv6EJwURVD2DuJCYJakwdrRQ556lI=";
+  paltypesSrc = pkgs.fetchFromGitHub {
+    owner = "deafdudecomputers";
+    repo = "PalworldSaveTools";
+    inherit rev;
+    sparseCheckout = [ "src/palsav/palsav/paltypes.py" ];
+    hash = "sha256-DB7T479PYSwyFCaY5EXfMgoHJJv2YODSymkkkVYJ3ZA=";
   };
 in
 pkgs.runCommand "palworld-type-hints-rs" { nativeBuildInputs = [ pkgs.python3 ]; } ''
-  python3 ${./gen-palworld-types.py} ${paltypesPy} > $out
+  python3 ${./gen-palworld-types.py} ${paltypesSrc}/src/palsav/palsav/paltypes.py > $out
 ''
